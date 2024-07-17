a151fa084bb90a972b18af44bfb11ee29db29bd5
nunzio <nunzio03@hotmail.it>
2024-07-17 17:29:16 +0200

    building px4 inside the docker image

--- docker_ws/build_SITL_ros2.sh
+++ docker_ws/build_SITL_ros2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-docker build --rm -t sitl_ros2:fuori -f Dockerfile.fuori .
+docker build --rm -t sitl_ros2:outer_volumes -f Dockerfile.outer_volumes .
--- run_SITL_ros2.sh
+++ run_SITL_ros2.sh
@@ -5,7 +5,6 @@ docker run --rm -it --privileged --ipc host \
 	--runtime nvidia --gpus all \
     -v ./scripts:/root/scripts \
     -v ./ros2_offboard_ws:/root/ros2_offboard \
-    -v ./px4_autopilot_ws:/root/px4_autopilot_ws \
 	-v /dev:/dev \
 	-v /tmp/.X11-unix/:/tmp/.X11-unix \
 	-v ~/.Xauthority:/root/.Xauthority \
@@ -13,4 +12,5 @@ docker run --rm -it --privileged --ipc host \
     -e DISPLAY=$DISPLAY \
     -w /root \
     --name ros2_sitl \
-    sitl_ros2:fuori bash 
+    sitl_ros2:outer_volumes bash 
+
