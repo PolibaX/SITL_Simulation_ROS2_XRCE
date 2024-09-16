cc9b26a7e488594ffa5878465aa02c02a329149a
nunzio <nunzio03@hotmail.it>
2024-09-16 11:31:44 +0200

    populating bridge launch

--- bridge_ws/src/gz_bridge_utilities/launch/custom_bridge.launch.py
+++ bridge_ws/src/gz_bridge_utilities/launch/custom_bridge.launch.py
@@ -7,12 +7,33 @@ def generate_launch_description():
             package='ros_gz_bridge',
             namespace='',
             executable='parameter_bridge',
-            name='sim',
+            name='bridge_poses',
             arguments=[
                 "/world/default/pose/info@geometry_msgs/msg/PoseArray[gz.msgs.Pose_V",
                 "--ros-args -r /world/default/pose/info:=/gazebo/model/state",
             ],
         ),
+        Node(
+            package='ros_gz_bridge',
+            namespace='',
+            executable='parameter_bridge',
+            name='bridge_tfs',
+            arguments=[
+                "/world/default/pose/info@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V",
+                "--ros-args -r /world/default/pose/info:=/tf",
+            ],
+        ),
+
+        Node(
+            package='ros_gz_image',
+            namespace='',
+            executable='image_bridge',
+            name='bridge_images',
+            arguments=["/camera",
+                       "/depth_camera",]
+
+        )
+        
         # Node(
         #     package='turtlesim',
         #     executable='mimic',
