938ddef2fc50b0283bb32402641d19255f980170
nunzio <nunzio03@hotmail.it>
2024-09-20 12:17:07 +0200

    bridging camera and pubblishing point cloud tf

--- bridge_ws/src/gz_bridge_utilities/launch/custom_bridge.launch.py
+++ bridge_ws/src/gz_bridge_utilities/launch/custom_bridge.launch.py
@@ -4,16 +4,15 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
  
-        # Node(
-        #     package='ros_gz_image',
-        #     namespace='',
-        #     executable='image_bridge',
-        #     name='bridge_images',
-        #     arguments=["/camera",
-        #             #    "/camera_info",
-        #                "/depth_camera",]
+        Node(
+            package='ros_gz_image',
+            namespace='',
+            executable='image_bridge',
+            name='bridge_images',
+            arguments=["/camera",
+                       "/depth_camera",]
 
-        # ),
+        ),
 
         Node(
             package='ros_gz_bridge',
@@ -24,24 +23,49 @@ def generate_launch_description():
                           "-r", "/world/default/pose/info:=/gazebo/model/state",]
         ),
 
-        # Node(
-        #     package='ros_gz_bridge',
-        #     namespace='',
-        #     executable='parameter_bridge',
-        #     name='bridge_camera_info',
-        #     arguments=["/depth_camera/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked",
-        #                "--ros-args",
-        #                   "-r", "/depth_camera/points:=/points",]
-        # ),
+        Node(
+            package='ros_gz_bridge',
+            namespace='',
+            executable='parameter_bridge',
+            name='bridge_camera_info',
+            arguments=["/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo","--ros-args"]
+        ),
 
-        # Node(
-        #     package='ros_gz_bridge',
-        #     namespace='',
-        #     executable='parameter_bridge',
-        #     name='bridge_tfs',
-        #     arguments=["/model/x500_depth_0/pose@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V","--ros-args",
-        #                   "-r", "/model/x500_depth_0/pose:=/tf",]
-        # ),
+
+
+
+        Node(
+            package='ros_gz_bridge',
+            namespace='',
+            executable='parameter_bridge',
+            name='bridge_points',
+            arguments=["/depth_camera/points@sensor_msgs/msg/PointCloud2[gz.msgs.PointCloudPacked",
+                       "--ros-args",
+                          "-r", "/depth_camera/points:=/gazebo/points",]
+        ),
+
+        Node(
+            package='ros_gz_bridge',
+            namespace='',
+            executable='parameter_bridge',
+            name='bridge_tfs',
+            arguments=["/model/x500_depth_0/pose@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V","--ros-args",
+                          "-r", "/model/x500_depth_0/pose:=/tf",]
+        ),
+
+        Node(
+            package='tf2_ros',
+            namespace = 'scan_to_map',
+            executable='static_transform_publisher',
+            arguments= ["0", "0", "0", "0", "0", "0", "camera_point_cloud", "x500_depth_0/OakD-Lite/base_link/StereoOV7251"]
+        ),
+
+        Node(
+            package='tf2_ros',
+            namespace = 'attach_point_cloud',
+            executable='static_transform_publisher',
+            arguments= ["0", "0", "0", "0", "0", "0", "x500_depth_0/OakD-Lite/base_link", "camera_point_cloud"]
+        ),
 
         # Node(
         #     package='depth_image_proc',
@@ -50,17 +74,8 @@ def generate_launch_description():
         #     name='point_cloud_xyz',
         #     remappings=[('image_rect', '/depth_camera'),
         #                 ('camera_info', '/camera_info'),
-        #                 # ('image', '/depth_camera')
+        #                  ('/points', '/im_proc/points'),
         #                 ]
         # )
         
-        # Node(
-        #     package='turtlesim',
-        #     executable='mimic',
-        #     name='mimic',
-        #     remappings=[
-        #         ('/input/pose', '/turtlesim1/turtle1/pose'),
-        #         ('/output/cmd_vel', '/turtlesim2/turtle1/cmd_vel'),
-        #     ]
-        # )
     ])
