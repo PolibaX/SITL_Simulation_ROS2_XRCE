b7f4e2a62a0ce8ac50fb55af562f74ad26352438
nunzio <nunzio03@hotmail.it>
2024-09-23 19:02:33 +0200

    fixed rover r1 sim

--- run_SITL_ros2.sh
+++ run_SITL_ros2.sh
@@ -6,7 +6,7 @@ docker run --rm -it --privileged --ipc host \
     -v ./scripts:/root/scripts \
     -v ./bridge_ws:/root/bridge_ws \
     -v ./ros2_offboard_ws:/root/ros2_offboard \
-    -v ./PX4-sim-patches/r1_model.sdf:/root/PX4-Autopilot/Tools/simulation/gz/models/r1_rover/model.sdf \
+    -v ./PX4-sim-patches/r1_rover:/root/PX4-Autopilot/Tools/simulation/gz/models/r1_rover/ \
     -v ./PX4-sim-patches/x500_model.sdf:/root/PX4-Autopilot/Tools/simulation/gz/models/x500/model.sdf \
     -v ./PX4-sim-patches/x500_depth_model.sdf:/root/PX4-Autopilot/Tools/simulation/gz/models/x500_depth/model.sdf \
     -v ./PX4-sim-patches/default_world_arena.sdf:/root/PX4-Autopilot/Tools/simulation/gz/worlds/default.sdf \
