a6cf59aee699e2e0a13f2215c63230461a53ddbc
walterbrescia <walter.brescia@poliba.it>
2025-07-15 16:08:24 +0200

    Fixing Accel fail: STALE error

--- scripts/start_sim_depth.sh
+++ scripts/start_sim_depth.sh
@@ -3,5 +3,5 @@ export PX4_UXRCE_DDS_NS=chotto
 cd /root/PX4-Autopilot
 # PX4_GZ_WORLD=<custom world in PX4-Autopilot/Tools/simulation/gz/worlds> make px4_sitl gz_x500_depth
 # PX4_GZ_MODEL_POSE="5. 2. 0. 0. 0. 0." 
-export PX4_GZ_MODEL_POSE="2. 5. 0. 0. 0. 0."
+export PX4_GZ_MODEL_POSE="2. 5. 0.15 0. 0. 0."
 make px4_sitl gz_x500_depth
